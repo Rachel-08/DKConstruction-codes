fab280252e5ed8acce852395f214c739a7421734
Lovina Rachel <80968800+Rachel-08@users.noreply.github.com>
2026-06-08 12:25:30 +0530

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -4,18 +4,19 @@ import { motion, type MotionValue } from "motion/react";
 import { Menu } from "lucide-react";
 
 type NavbarProps = {
-  navOpacity: MotionValue<number>;
-  navY: MotionValue<number>;
+  showNavbar: boolean;
+  // navOpacity: MotionValue<number>;
+  // navY: MotionValue<number>;
 };
 
-export default function Navbar({ navOpacity, navY }: NavbarProps) {
+
+export default function Navbar({ showNavbar }: NavbarProps) {
   return (
     <motion.header
-      style={{
-        opacity: navOpacity,
-        y: navY,
-      }}
-      className="pointer-events-auto fixed left-0 top-0 z-[999] w-full border-b border-black/10 bg-[#f5f2eb]/80 backdrop-blur-md"
+      initial={false}
+      animate={showNavbar ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
+      transition={{ duration: 0.35, ease: "easeInOut" }}
+      className="pointer-events-auto fixed left-0 top-0 z-[999] w-full border-b border-black/10 bg-white/50 backdrop-blur-md"
     >
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-5">
         {/* LOGO */}
@@ -45,11 +46,11 @@ export default function Navbar({ navOpacity, navY }: NavbarProps) {
 
               <span className="absolute left-0 -bottom-1 h-[1px] w-0 bg-black transition-all duration-300 group-hover:w-full" />
 
-              <span className="absolute left-0 top-0 h-2 w-2 border-l border-t border-black opacity-0 transition-all duration-300 group-hover:opacity-100" />
+              {/* <span className="absolute left-0 top-0 h-2 w-2 border-l border-t border-black opacity-0 transition-all duration-300 group-hover:opacity-100" /> */}
 
-              <span className="absolute right-0 bottom-0 h-2 w-2 border-r border-b border-black opacity-0 transition-all duration-300 group-hover:opacity-100" />
+              {/* <span className="absolute right-0 bottom-0 h-2 w-2 border-r border-b border-black opacity-0 transition-all duration-300 group-hover:opacity-100" /> */}
 
-              <span className="absolute inset-0 rounded-md opacity-0 transition duration-300 group-hover:opacity-100 group-hover:shadow-[0_4px_12px_rgba(0,0,0,0.08)]" />
+              <span className="absolute inset-0 rounded-md opacity-0 transition duration-300 group-hover:opacity-100 " />
             </a>
           ))}
         </nav>
